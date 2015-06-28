ec278b0dae301ed1671528d072f576f2c1e6215e
Harsh Gupta <gupta.harsh96@gmail.com>
2015-06-29 01:13:06 +0530

    Fix js filename in gender_by_dob
    Phew, lesson, never copy paste code unless it is more than 10 line, keep a
    split window and type it, it is will ensure you don't copy the wrong parts.
    
    Today I wrote code to plot gender by dob and it worked fine but then I saw that
    gender by country plot did not show up. I investigated more and it turned out
    the ID's of the plots in the script tag in html file and that of the js file
    did not match. I cleared the cache, read the program, reread the program but
    couldn't find anything. Then I took another clone of the repo and ran `nikola
    build` on it, there was error "output/scripts/gender_by_country.js" doesn't
    exist, I knew it could be fixed by creating an empty scripts directory in
    output, but I didn't and instead didn't use gender_by_country in conf. Then ran
    `nikola build` again. Again the same error, then git grep `gender_by_country`,
    ola found it, both gender_by_dob and gender_by_country were writing to the same
    js file -_-, and this was because I copied it from gender_by_country. Okay,
    probably the lesson should be, don't use magic string and if you need to copy a
    part of code better create a function out of it and call it.
    
    Signed-off-by: Harsh Gupta <gupta.harsh96@gmail.com>

--- gender_by_dob.py
+++ gender_by_dob.py
@@ -46,7 +46,7 @@ def create_gender_by_dob_plot():
 
     p = gridplot([[p1], [p2]], toolbar_location=None)
 
-    js_file_name = "gender_by_country.js"
+    js_file_name = "gender_by_dob.js"
     script_path = "./scripts/"
     output_path = "output/scripts/"
     js, tag = autoload_static(p, CDN, script_path + js_file_name)
@@ -59,21 +59,3 @@ def create_gender_by_dob_plot():
 
 if __name__ == "__main__":
     print(create_gender_by_dob_plot())
-
-# fig, (pltf, pltb) = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(9, 6))
-# dox.plot(kind='line',  cmap='Paired', linewidth=2, ax=pltf)
-# pltf.set_xlim((1400, 2014))
-# pltf.set_ylim((0, 0.7))
-# pltf.yaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, y: '{}%'.format(int(x*100))))
-# pltf.set_title('Female ratio')
-# pltf.legend(('Date of Birth', 'Date of Death'), loc=4, bbox_to_anchor=(1.25, -0.25))
-#
-# nonbindox.plot(kind='line',  cmap='Paired', linewidth=2, ax=pltb, legend=False)
-# pltb.set_xlim((1400, 2014))
-# pltb.yaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, y: '{}%'.format(x*100)))
-# pltb.set_title('Non-binary Ratio')
-#
-# fig.suptitle('Composition of Wikidata Genders in Modern History', fontsize=24)
-# fig.subplots_adjust(top=0.87)
-#
-# plt.savefig('./output/plots/gender_by_dob.png')
