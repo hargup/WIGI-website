7295871f2f6b4b579004330b7ea147184003657c
Vivek Rai <vivekraiiitkgp@gmail.com>
2016-01-05 14:18:46 +0530

    Add Fem(%) column
    Should close #91
    
    Signed-off-by: Vivek Rai <vivekraiiitkgp@gmail.com>

--- plots/gender_by_culture.py
+++ plots/gender_by_culture.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import pandas as pd
 from bokeh._legacy_charts import Bar
 from bokeh.models import NumeralTickFormatter
+from numpy import round
 
 from .utils import write_plot, read_data
 
@@ -17,13 +18,16 @@ def plot(newest_changes):
 
     df['total'] = df.sum(axis=1)
     df['nonbin'] = df['total'] - df['male'] - df['female']
-    df['fem_per'] = df['female'] / (df['total'])
-    df['nonbin_per'] = df['nonbin'] / df['total']
-    df['fem_per_million'] = df['fem_per'] * 1000000
-    df['nonbin_per_million'] = df['nonbin_per'] * 1000000
-    dfs = df.sort_values('female')
+    df['fem_per'] = df['female'] / (df['total']) * 100
+    df['nonbin_per'] = df['nonbin'] / df['total'] * 100
+    df['fem_per_million'] = df['fem_per'] * 10000
+    df['nonbin_per_million'] = df['nonbin_per'] * 10000
 
-    interesante = ['female', 'male', 'nonbin', 'total']
+    # sort, process
+    dfs = df.sort_values('female')
+    dfs = round(dfs, decimals=2)
+
+    interesante = ['female', 'male', 'nonbin', 'total', 'fem_per']
 
     p = Bar(dfs[['female', 'male']],
             stacked=True,
@@ -35,7 +39,7 @@ def plot(newest_changes):
 
     p._yaxis.formatter = NumeralTickFormatter(format='0,0')
     htmltable = dfs[interesante].sort_values('female', ascending=False)
-    htmltable.columns = ['Female', 'Male', 'Non Binary', 'Total']
+    htmltable.columns = ['Female', 'Male', 'Non Binary', 'Total', 'Female (%)']
     top_rows = htmltable.head(10)
     bottom_rows = htmltable[::-1].head(10)
     table = [top_rows, bottom_rows]
